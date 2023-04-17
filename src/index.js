fe91c5083dbf6c9b95292e9b84109932f79fec55
Darya Hudzovska <102170750+bestboost@users.noreply.github.com>
2023-04-17 18:18:18 +0300

    fin

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import name from './module-1';
+import './css/common.scss';
 
 console.log("name:", name); 
 console.log("Hello, I`m Webpack!");
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
      mode: 'development',
@@ -7,12 +10,30 @@ module.exports = {
           path: path.resolve(__dirname, 'build'),
           filename: 'my-bundle.js',
      },
+     module: {
+          rules: [
+               {
+                    test: /\.s[ac]ss$/i,
+                    use: ['style-loader', 
+                    MiniCssExtractPlugin.loader, 
+                    'css-loader', 
+                    'sass-loader'],
+                  },
+                  {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: ["babel-loader"],
+                  },
+          ],
+     },
+     plugins: [new HtmlWebpackPlugin({template: 'src/index.html'}),
+     new MiniCssExtractPlugin({filename: 'style.css'}),
+     new CleanWebpackPlugin(),
+],
+    
      devServer: {
           port: 4444,
-          open: true, 
-          client: {
-               logging: 'error',
-             },
+          open: true,
+          stats: 'errors-only',
      },
-     stats: 'errors-only',
 };  
